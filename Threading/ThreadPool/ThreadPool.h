43eafced8aad87348360651fdecbdfd90ce10897
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2019-01-28 20:57:23 +0800

    Removed some mutex lock uses
    Removed the heavy overhead of mutex locking when polling , pushing and popping in the thread pool.
    
    But also broke many other parts and need much fixing.

--- Threading/ThreadPool/ThreadPool.h
+++ Threading/ThreadPool/ThreadPool.h
@@ -18,6 +18,8 @@
 extern unsigned all_available;
 extern const std::thread::id main_thread_id;
 
+unsigned GetThreadIndex(unsigned init_index = -1);
+
 class ThreadPool {
 public:
 	class TaskQueue {
@@ -35,11 +37,19 @@ public:
 	public:
 		~TaskQueue();
 		void clear();
+		//Currently the lock is implemented with a atomic bool.
+		//If exception happens, the boolean is not unlocked and may result in a deadlock
+		//that eats up system resource.
+		//Non-Exception-Safe!
 		void push(const std::function<bool()>& function);
 		std::function<bool()> pop();
-		bool empty();
-		size_t waiting();
 		void append(const TaskQueue& src);
+		//These functions do not use the in_use for spin lock
+		//Thus, are prone to error if race condition occurs 
+		bool empty();
+		bool empty_strict();
+		size_t waiting();
+		size_t waiting_strict();
 	};
 	ThreadPool();
 	ThreadPool(unsigned num);
@@ -50,15 +60,17 @@ public:
 	unsigned AddThread();
 	bool RemThread(unsigned ID);
 
+	//No mutex involved in Poll functions and Schedule functions!
 	void PollAllTasks(float timeout = 0);
 	void PollTasks();
-	void PushTask(const std::function<bool()> added);
+	void PushTask(const std::function<bool()>& added);
 	void ScheduleTasks(const TaskQueue& added);
 	void ClearSchedule();
 	void ClearAllTasks();
 
 	bool ThreadTasks(const TaskQueue& added,unsigned thread);
-	bool ThreadWaiting(unsigned thread);
+	bool ThreadAvailable(unsigned thread);
+	size_t ThreadTODO(unsigned thread);
 	size_t QuerieSchedule();
 	size_t QuerieThread(unsigned thread);
 	unsigned WaitAll(float timeout = 0);
@@ -71,10 +83,19 @@ private:
 		std::mutex mtx{};
 		std::condition_variable cv{};
 		TaskQueue queue{};
-		std::atomic<bool> finished = true;
+		bool waiting = false;
 		bool active = true;
+		bool finished = true;
 		size_t num_done = 0;
 		bool ready_exit = false;
+		//The structure is exclusive to the executing thread and values 
+		//are indicators for current execution state,
+		//with the exception of the pool adding things from other thread.
+		//setting this can allow a spin-lock when doing light-weight job.
+		//Lock both this and the mutex for long-running dispatchs
+		//Light-weight and fast changes should use this indicator instead.
+		std::atomic<bool> modifying = false;
+		unsigned index;
 	};
 	passed_vals* const states;
 	std::thread* const threads;
