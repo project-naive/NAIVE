f75efff185e8a4001aa6059e21341e2c467cbca7
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2019-03-11 22:09:09 +0800

    Simple state updated

--- Samples/2048/2048/States/Simple/Simple.cpp
+++ Samples/2048/2048/States/Simple/Simple.cpp
@@ -49,12 +49,12 @@ namespace States {
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, GLsizei(rb_width), GLsizei(rb_height), 0, GL_RGBA, GL_FLOAT, nullptr);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glGenRenderbuffers(1, &texture_rbo);
-		glBindRenderbuffer(GL_RENDERBUFFER, texture_rbo);
-		glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA32F, GLsizei(rb_width), GLsizei(rb_height));
-		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, texture_rbo);
+//		glGenRenderbuffers(1, &texture_rbo);
+//		glBindRenderbuffer(GL_RENDERBUFFER, texture_rbo);
+//		glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA32F, GLsizei(rb_width), GLsizei(rb_height));
+//		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, texture_rbo);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
-		GLenum DrawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
+		GLenum DrawBuffers[1] = { GL_COLOR_ATTACHMENT0  };
 		glDrawBuffers(1, DrawBuffers);
 		Engine::Graphics::WindowInfo info = Engine::Graphics::Contexts::Default::default_window();
 		info.Basic_Info.height = 1024;
@@ -65,7 +65,7 @@ namespace States {
 		info.Extended_Info.Display.SwapInterval = 0;
 		info.Extended_Info.Context.ContextInfo.debug = true;
 		info.Extended_Info.Context.FramebufferInfo.double_buffer = true;
-		ContextIDs[1] = Managers.ContextManager.GenContext(info, Managers.ContextManager.GetCurrent());
+	//	ContextIDs[1] = Managers.ContextManager.GenContext(info, Managers.ContextManager.GetCurrent());
 		//Do stuff with the other context...
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		size_t texture_shader = given_managers.ShaderManager.addGeneric(*(new ::State_2048::Shaders::TextureQuad(given_managers.ShaderManager)));
@@ -106,6 +106,7 @@ namespace States {
 	}
 
 	void Simple::Draw() {
+		Managers.ContextManager.SetContext(ContextIDs[0]);
 		std::chrono::steady_clock::time_point cur = std::chrono::steady_clock::now();
 		size_t width, height;
 		if (is_refresh_frame) {
@@ -119,7 +120,8 @@ namespace States {
 			Managers.ContextManager.SetContext(ContextIDs[0]);
 			FPS = FPS_cache;
 			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, texture_fbo);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 			glViewport(0, 0, GLsizei(rb_width), GLsizei(rb_height));
 			PrivateModels.TriangleModel.Begin();
 			PrivateModels.TriangleModel.Draw();
@@ -137,29 +139,40 @@ namespace States {
 			lowerFPS = 10000;
 			Managers.TextManager.renderText(oss.str().c_str(), glm::vec2(40.0f, 800.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0.4f);
 			//Do stuff with the rendered render buffer
+			/*
 			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 			Managers.ContextManager.GetCurrentResolution(width, height);
 			glViewport(0, 0, GLsizei(width), GLsizei(height));
 			glBlitFramebuffer(0, 0, GLint(rb_width), GLint(rb_height),
-							  0, 0, GLint(width), GLint(height),
-							  GL_COLOR_BUFFER_BIT, GL_LINEAR);
+				0, 0, GLint(width), GLint(height),
+				GL_COLOR_BUFFER_BIT, GL_LINEAR);
 			Managers.ContextManager.SwapBuffers();
+			*/
+			/*
+			Managers.ContextManager.GetCurrentResolution(width, height);
+			glBlitFramebuffer(0, 0, GLint(rb_width), GLint(rb_height),
+				0, 0, GLint(width), GLint(height),
+				GL_COLOR_BUFFER_BIT, GL_LINEAR);
+				*/
 		}
-		if(ContextIDs[1]!=size_t(-1)) {
-			Managers.ContextManager.SetContext(ContextIDs[1]);
+	//	if(ContextIDs[1]!=size_t(-1)) {
+	//		Managers.ContextManager.SetContext(ContextIDs[1]);
 			Managers.ContextManager.GetCurrentResolution(width, height);
 			if (is_refresh_frame) {
+				glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 				glViewport(0, 0, GLsizei(width), GLsizei(height));
-			} else {
+			}
+			else {
 				lowerFPS = std::min(FPS, lowerFPS);
 			}
+			glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			CubeModel->Begin();
 			CubeModel->Draw();
-		//	TextureModel->Begin();
-		//	TextureModel->Draw();
+//				TextureModel->Begin();
+//				TextureModel->Draw();
 			Managers.ContextManager.SwapBuffers();
-		}
+	//	}
 	}
 	void Simple::Update() {
 		std::chrono::high_resolution_clock::time_point cur = std::chrono::high_resolution_clock::now();
@@ -189,7 +202,7 @@ namespace States {
 	}
 
 	bool Simple::notifyContextClose(size_t ID) {
-		if (ID == ContextIDs[0]) {
+		if (ID == ContextIDs[0] || ID == ContextIDs[1]) {
 			Managers.ContextManager.DelContext(ID);
 			running = false;
 			return false;
