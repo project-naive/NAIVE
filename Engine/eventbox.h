82fb01b936cc7ec87d79d39f0185262c8915283f
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2019-07-19 12:21:05 +0800

    Basic event box implementation done

--- Engine/eventbox.h
+++ Engine/eventbox.h
@@ -27,11 +27,11 @@ public:
 	};
 protected:
 	struct communication {
-		comm_state state;							//the current state of communication
+		uint32_t state;							//the current state of communication
 		uint32_t type;
 		rigtorp::SPSCQueue<message>* events;	//the queue to recieve message from
 		rigtorp::SPSCQueue<message>* post;		//the queue to send message to
-		int64_t id;
+		uint64_t id;
 	};
 private:
 #define COM_SEG ((4096 - 8) / sizeof(eventbox::communication))
@@ -45,13 +45,16 @@ private:
 	communication** recycled = nullptr;
 	size_t num_recycled = 0;
 	size_t recycle_size = 0;
+	struct queue_block {
+		queue_block* next;
+		char pad[sizeof(rigtorp::SPSCQueue<message>) - sizeof(queue_block*)];
+	};
+	queue_block* queue_areas = nullptr;
 	rigtorp::MPMCQueue<rigtorp::SPSCQueue<message>*> applications;
 	bool recycle_comm_slot(communication* com) {
 #ifndef NDEBUG
 		assert(com);
 #endif
-		com->id = -1;
-		com->state = comm_state::COMM_NONE;
 		if (num_recycled == recycle_size) {
 			size_t new_size = size_t(recycle_size*1.618) + 1;
 			communication** new_place = (communication**)realloc(recycled, new_size * sizeof(communication*));
@@ -63,7 +66,77 @@ private:
 		}
 		recycled[num_recycled] = com;
 		++num_recycled;
+		com->id = 0;
+		com->state = uint32_t(comm_state::COMM_NONE);
 		return true;
+	}
+	void recycle_queue_block(rigtorp::SPSCQueue<message>* p) {
+		if (!queue_areas) {
+			queue_areas = (queue_block*)p;
+			queue_areas->next = nullptr;
+			return;
+		}
+		((queue_block*)p)->next = queue_areas;
+		queue_areas= (queue_block*)p;
+	}
+	rigtorp::SPSCQueue<message>* assign_queue_block() {
+		if (queue_areas) {
+			rigtorp::SPSCQueue<message>* ptr = (rigtorp::SPSCQueue<message>*)queue_areas;
+			queue_areas = queue_areas->next;
+			return ptr;
+		}
+		return (rigtorp::SPSCQueue<message>*)malloc(sizeof(rigtorp::SPSCQueue<message>));
+	}
+	void clear_queue_areas(queue_block* node) {
+		if (node->next) {
+			clear_queue_areas(node->next);
+		}
+		free(node);
+	}
+	void remove_recycle(communication* start, communication* end) {
+		assert(start);
+		assert(end);
+		int64_t size = end - start;
+		for (size_t i = 0; i < num_recycled; ++i) {
+			if (recycled[i] > start && recycled[i] < end) {
+				--num_recycled;
+				for (size_t j = i; j < num_recycled; ++j) {
+					recycled[i] = recycled[i + 1];
+				}
+			}
+		}
+	}
+	bool clean_comms(comm_node* node, size_t recurse = 0) {
+		bool next_okay;
+		if (node->next) {
+			next_okay = clean_comms(node->next, recurse + 1);
+		}
+		else { 
+			next_okay = true;
+		}
+		bool okay = next_okay;
+		communication* start = nullptr;
+		int64_t num_search = 0;
+		if (node->next) num_search = COM_SEG - 1;
+		else num_search = (num_used - 1) % COM_SEG;
+		for (int64_t i = num_search; i >= 0; --i) {
+			if (node->comms[i].state) {
+				start = &(node->comms[i]);
+				okay = false;
+				break;
+			}
+		}
+		if (okay) {
+			remove_recycle(node->comms - 1, node->comms + COM_SEG);
+			num_comm -= COM_SEG;
+			mi_free_aligned(node, 4096);
+			return okay;
+		}
+		if (next_okay) {
+			remove_recycle(start, node->comms + COM_SEG);
+			node->next = nullptr;
+			return okay;
+		}
 	}
 protected:
 	communication* connect_accept(const message& msg) {
@@ -74,7 +147,7 @@ protected:
 			msg.callback->emplace(message{ EVENT_REGISTER_FAIL, msg.type_specific, (rigtorp::SPSCQueue<message>*)this, msg.callback });
 			return nullptr;
 		}
-		*comm = { comm_state::COMM_ACKED, msg.type_specific,(rigtorp::SPSCQueue<message>*)msg.msg , msg.callback };
+		*comm = { uint32_t(comm_state::COMM_ACKED), msg.type_specific,(rigtorp::SPSCQueue<message>*)msg.msg , msg.callback };
 		comm->post->emplace(message{ EVENT_REGISTER_ACK, msg.type_specific, comm->events, comm->post });
 		return comm;
 	}
@@ -84,46 +157,61 @@ protected:
 		assert(msg.type_global == EVENT_UNREGISTER);
 		assert(msg.callback == com.post);
 		assert(msg.msg == com.events);
-		com.state = comm_state::COMM_NONE;
+		if (!recycle_comm_slot(&com)) {
+			com.post->emplace(message{
+				EVENT_UNREGISTER_FAIL, msg.type_specific,com.events, com.post });
+			return false;
+		}
+		com.state = uint32_t(comm_state::COMM_NONE);
 		com.post->emplace(message{
 			EVENT_UNREGISTER_ACK, msg.type_specific,(rigtorp::SPSCQueue<message>*)this, com.post });
-		return recycle_comm_slot(&com);
+		return true;
 	}
 	void connect_establish(const message& msg, communication& com) {
 		assert(msg.callback == com.post);
 		assert(msg.msg == com.events);
-		assert(com.state == comm_state::COMM_PENDING);
+		assert(com.state == uint32_t(comm_state::COMM_PENDING));
 		assert(msg.type_global == EVENT_REGISTER_ACK);
-		com.state = comm_state::COMM_EST;
+		com.state = uint32_t(comm_state::COMM_EST);
 	}
 	void connect_accept_cancel(const message& msg, communication& com) {
-		assert(com.state == comm_state::COMM_ACKED);
+		assert(com.state == uint32_t(comm_state::COMM_ACKED));
 		assert(msg.type_global == EVENT_REGISTER_CANCEL);
 		assert(msg.msg == com.events);
 		assert(msg.callback == com.post);
-		delete com.post;
-		delete com.events;
+		(*com.post).~SPSCQueue<message>();
+		recycle_queue_block(com.post);
+		(*com.events).~SPSCQueue<message>();
+		recycle_queue_block(com.events);
 		recycle_comm_slot(&com);
 	}
 	void disconnect_done(const message& msg, communication& com) {
 		assert(msg.type_global == EVENT_UNREGISTER_ACK);
 		assert(msg.msg == com.events);
-		delete com.events;
-		delete com.post;
-		com.state = comm_state::COMM_NONE;
+		com.post->~SPSCQueue();
+		recycle_queue_block(com.post);
+		com.events->~SPSCQueue();
+		recycle_queue_block(com.events);
+		com.state = uint32_t(comm_state::COMM_NONE);
 		recycle_comm_slot(&com);
 	}
 	void connect_accept_fail(const message& msg, communication& com) {
 		assert(msg.msg == com.events);
-		assert(com.state == comm_state::COMM_PENDING);
+		assert(com.state == uint32_t(comm_state::COMM_PENDING));
 		((eventbox*)msg.callback)->applications.emplace(com.events);
+	}
+	void disconnect_accept_fail(const message& msg, communication& com) {
+		assert(msg.callback == com.post);
+		assert(msg.msg == com.events);
+		assert(msg.type_global == EVENT_UNREGISTER_FAIL);
+		com.post->emplace(message{ EVENT_UNREGISTER, msg.type_specific, com.events,com.post });
 	}
 	communication* assign_port() {
 		if (num_recycled) {
 			assert(head);
 			--num_recycled;
 			communication* ptr = recycled[num_recycled];
-			ptr->state = comm_state::COMM_NONE;
+			ptr->state = uint32_t(comm_state::COMM_NONE);
 			ptr->id = id_max;
 			++id_max;
 			return ptr;
@@ -141,7 +229,7 @@ protected:
 			size_t index = num_used % COM_SEG;
 			++num_used;
 			communication* ptr = &(node->comms[index]);
-			ptr->state = comm_state::COMM_NONE;
+			ptr->state = uint32_t(comm_state::COMM_NONE);
 			ptr->id = id_max;
 			++id_max;
 			return ptr;
@@ -149,7 +237,7 @@ protected:
 		comm_node* new_page = (comm_node*)mi_aligned_alloc(4096, 4096);
 		if (!new_page) {
 			//deal with failed allocation
-			return false;
+			return nullptr;
 		}
 		new_page->next = nullptr;
 		comm_node* node = head;
@@ -163,15 +251,67 @@ protected:
 		num_comm += COM_SEG;
 		++num_used;
 		communication* ptr = &(new_page->comms[0]);
-		ptr->state = comm_state::COMM_NONE;
+		ptr->state = uint32_t(comm_state::COMM_NONE);
 		ptr->id = id_max;
 		++id_max;
 		return ptr;
 	}
 //	virtual void process_single_event(const message& msg, communication& com) = 0;
-	int64_t id_max = 0;	//id = -1 means invalid
+	uint64_t id_max = 1;	//id = 0 means invalid
+	int process_single_event(const eventbox::message& msg, communication& com) {
+		switch (msg.type_global) {
+			case EVENT_REGISTER:
+#ifndef NDEBUG
+				std::cout << "EVENT_REGISTER GOT.\n";
+#endif
+				return 0;
+			case EVENT_REGISTER_CANCEL:
+#ifndef NDEBUG
+				std::cout << "EVENT_REGISTER_CANCEL GOT.\n";
+#endif
+				connect_accept_cancel(msg, com);
+				return 2;
+			case EVENT_REGISTER_ACK:
+#ifndef NDEBUG
+				std::cout << "EVENT_REGISTER_ACK GOT.\n";
+#endif
+				connect_establish(msg, com);
+				return 0;
+			case EVENT_REGISTER_FAIL:
+#ifndef NDEBUG
+				std::cout << "EVENT_REGISTER_FAIL GOT.\n";
+#endif
+				connect_accept_fail(msg, com);
+				return 1;
+			case EVENT_UNREGISTER:
+#ifndef NDEBUG
+				std::cout << "EVENT_UNREGISTER GOT.\n";
+#endif
+				disconnect_accept(msg, com);
+				return 2;
+			case EVENT_UNREGISTER_CANCEL:
+#ifndef NDEBUG
+				std::cout << "EVENT_UNREGISTER_CANCEL GOT.\n";
+#endif
+				abort();
+				break;
+			case EVENT_UNREGISTER_ACK:
+#ifndef NDEBUG
+				std::cout << "EVENT_UNREGISTER_ACK GOT.\n";
+#endif
+				disconnect_done(msg, com);
+				return 2;
+			case EVENT_UNREGISTER_FAIL:
+#ifndef NDEBUG
+				std::cout << "EVENT_UNREGISTER_FAIL GOT.\n";
+#endif
+				disconnect_accept_fail(msg, com);
+				return 1;
+		}
+		return static_cast<T*>(this)->process_specific_event(msg, com);
+	};
 public:
-	enum : uint64_t{
+	enum :uint32_t {
 		EVENT_REGISTER,
 		EVENT_REGISTER_CANCEL,
 		EVENT_REGISTER_ACK,
@@ -179,9 +319,10 @@ public:
 		EVENT_UNREGISTER,
 		EVENT_UNREGISTER_CANCEL,	//impossible
 		EVENT_UNREGISTER_ACK,
-		EVENT_UNREGISTER_FAIL,		//not implemented
+		EVENT_UNREGISTER_FAIL,
 		EVENT_CANNOT_PROCESS_ALL,
-		EVENT_REGISTER_PRIORITY		//not implemented
+		EVENT_REGISTER_PRIORITY,	//not implemented
+		EVENT_BASE_LAST
 	};
 	struct message {
 		uint32_t type_global;
@@ -192,7 +333,7 @@ public:
 	struct connection_info {
 		void* handle;	//handle of a communication, internally a communication*
 #ifndef NDEBUG
-		int64_t id;		//for verification
+		uint64_t id;		//for verification
 #endif
 	};
 	//call this in the producer instance before anything else.
@@ -201,13 +342,21 @@ public:
 		uint32_t self_events = 100, uint32_t peer_events = 100) {
 		communication* comm = assign_port();
 #ifndef NDEBUG
-		if (!comm) return { nullptr, -1 };
+		if (!comm) return { nullptr, 0 };
 #else
 		if (!comm) return { nullptr };
 #endif
-		comm->state = comm_state::COMM_PENDING;
-		comm->events = new rigtorp::SPSCQueue<message>(self_events);
-		comm->post = new rigtorp::SPSCQueue<message>(peer_events);
+		comm->state = uint32_t(comm_state::COMM_PENDING);
+		comm->events = assign_queue_block();
+		if (!comm->events) {
+			//failed allocation
+		}
+		new (comm->events) rigtorp::SPSCQueue<message>(self_events);
+		comm->post = assign_queue_block();
+		if (!comm->post) {
+			//failed allocation
+		}
+		new (comm->post) rigtorp::SPSCQueue<message>(peer_events);
 		comm->type = self_type;
 		comm->post->emplace(message{ EVENT_REGISTER, peer_type,comm->events, comm->post });
 		//current implementation cannot handle failed registration
@@ -223,7 +372,8 @@ public:
 		assert(head);
 		comm_node* node = head;
 		while (node) {
-			if (((communication*)info.handle) - node->comms < COM_SEG)
+			if ((((communication*)info.handle) - node->comms)>=0 &&
+				(((communication*)info.handle) - node->comms) < COM_SEG)
 				break;
 			node = node->next;
 		}
@@ -235,25 +385,26 @@ public:
 		//verify that the info has valid id
 #ifndef NDEBUG
 		verify_connection_info(info);
-		assert((((communication*)info.handle)->state == comm_state::COMM_ACKED)
-			|| (((communication*)info.handle)->state == comm_state::COMM_EST));
+		assert((((communication*)info.handle)->state == uint32_t(comm_state::COMM_ACKED))
+			|| (((communication*)info.handle)->state == uint32_t(comm_state::COMM_EST)));
 #endif
-		((communication*)info.handle)->state = comm_state::COMM_ENDWAIT;
+		((communication*)info.handle)->state = uint32_t(comm_state::COMM_ENDWAIT);
 		((communication*)info.handle)->post
 			->emplace(message{EVENT_UNREGISTER,((communication*)info.handle)->type,
 				((communication*)info.handle)->events, ((communication*)info.handle)->post});
 	}
-	bool process_events(size_t max_event = SIZE_MAX) {
+	void poll_incomming(const eventbox::message& msg) {
 		//accept incomming connections
 		rigtorp::SPSCQueue<message>* comming;
 		while (applications.try_pop(comming)) {
 			//process cur
 			communication* com = connect_accept(*(comming->front()));
 			if (com)
-				static_cast<T*>(this)->register_incomming(com);
-//			comming->pop();
+				static_cast<T*>(this)->register_incomming(msg, *com);
+			//			comming->pop();
 		}
-		//process events
+	}
+	bool process_comms(size_t max_event = SIZE_MAX) {
 		bool rtn = true;
 		comm_node* node = head;
 		uint32_t i = 0;
@@ -267,7 +418,7 @@ public:
 						if (!msg) break;
 						//process msg
 						bool end = false;
-						switch (static_cast<T*>(this)->process_single_event(*msg, com)) {
+						switch (process_single_event(*msg, com)) {
 							case 0:
 								break;
 							case 1:
@@ -278,7 +429,7 @@ public:
 								end = true;
 								break;
 						}
-						if(end)
+						if (end)
 							break;
 						com.events->pop();
 					}
@@ -292,6 +443,11 @@ public:
 			node = node->next;
 		}
 		return rtn;
+	}
+	bool process_events(size_t max_event = SIZE_MAX) {
+		poll_incomming();
+		//process events
+		return process_comms(max_event);
 	};
 	void connect_cancel(const connection_info& con) {
 #ifndef NDEBUG
@@ -303,17 +459,120 @@ public:
 			((communication*)con.handle)->type, ((communication*)con.handle)->events, ((communication*)con.handle)->post });
 		recycle_comm_slot((communication*)con.handle);
 	}
-	static comm_state get_state(const connection_info& info) {
+	static uint32_t get_state(const connection_info& info) {
 		return ((communication*)info.handle)->state;
 	}
 	static uint32_t get_type(const connection_info& info) {
 		return ((communication*)info.handle)->type;
+	}
+	bool try_post_event(const connection_info& info, message&& message) {
+		assert(info.handle);
+		communication& com = *((communication*)info.handle);
+#ifndef NDEBUG
+		assert(com.id == info.id);
+#endif
+		return com.post->try_emplace(message);
+	}
+	//may block
+	void post_event(const connection_info& info, message&& message) {
+		assert(info.handle);
+		communication& com = *((communication*)info.handle);
+#ifndef NDEBUG
+		assert(com.id == info.id);
+#endif
+		com.post->emplace(message);
 	}
 	eventbox(const eventbox&) = delete;
 	eventbox(eventbox&&) = delete;
 	eventbox& operator=(const eventbox&) = delete;
 	eventbox& operator=(eventbox&&) = delete;
 	eventbox(size_t listen_size = 16) :applications(listen_size) {};
+	bool resize_recycle() {
+		bool cleaned = false;
+		if (recycle_size) {
+			if (num_recycled)
+				recycled = (communication**)realloc(recycled, num_recycled * sizeof(communication*));
+			else {
+				free(recycled);
+				cleaned = true;
+			}
+		}
+		return cleaned;
+	}
+	bool collect_no_recycle() {
+		if (queue_areas) {
+			clear_queue_areas(queue_areas);
+			queue_areas = nullptr;
+		}
+		if (head) {
+			if (clean_comms(head))
+				head = nullptr;
+		}
+		if (!head) {
+			return true;
+		}
+		return false;
+	}
+	bool collect() {
+		return collect_no_recycle() && resize_recycle();
+	}
+	//applications not dealt with
+	~eventbox() {
+		do {
+			while (!collect_no_recycle()) {
+				size_t node_index = num_used / COM_SEG;
+				size_t index = num_used % COM_SEG;
+				comm_node* node = head;
+				for (size_t i = 0; i < node_index; ++i) {
+					for (size_t j = 0; j < COM_SEG; ++j) {
+						switch (node->comms[j].state) {
+							case uint32_t(comm_state::COMM_NONE):
+							case uint32_t(comm_state::COMM_ENDWAIT):
+								break;
+							case uint32_t(comm_state::COMM_PENDING):
+								node->comms[j].post
+									->emplace(message{ EVENT_REGISTER_CANCEL,node->comms[j].type,
+										node->comms[j].events, node->comms[j].post });
+								recycle_comm_slot(&(node->comms[j]));
+								break;
+							case uint32_t(comm_state::COMM_ACKED):
+							case uint32_t(comm_state::COMM_EST):
+								node->comms[j].state = uint32_t(comm_state::COMM_ENDWAIT);
+								node->comms[j].post
+									->emplace(message{ EVENT_UNREGISTER,node->comms[j].type,
+										node->comms[j].events, node->comms[j].post });
+								break;
+							default:
+								abort();
+						}
+					}
+					assert(node->next);
+					node = node->next;
+				}
+				for (size_t j = 0; j < index; ++j) {
+					switch (node->comms[j].state) {
+					case uint32_t(comm_state::COMM_NONE):
+					case uint32_t(comm_state::COMM_ENDWAIT):
+						break;
+					case uint32_t(comm_state::COMM_PENDING):
+						node->comms[j].post
+							->emplace(message{ EVENT_REGISTER_CANCEL,node->comms[j].type,
+								node->comms[j].events, node->comms[j].post });
+						recycle_comm_slot(&(node->comms[j]));
+						break;
+					case uint32_t(comm_state::COMM_ACKED):
+					case uint32_t(comm_state::COMM_EST):
+						node->comms[j].state = uint32_t(comm_state::COMM_ENDWAIT);
+						node->comms[j].post
+							->emplace(message{ EVENT_UNREGISTER,node->comms[j].type,
+								node->comms[j].events, node->comms[j].post });
+						break;
+					}
+				}
+				process_comms();
+			}
+		} while (!resize_recycle());
+	}
 };
 
 //constexpr size_t com_seg = COM_SEG;
@@ -323,47 +582,17 @@ public:
 #include <iostream>
 class test: public eventbox<test> {
 public:
-	int process_single_event(const eventbox::message& msg, communication& com) {
-		switch (msg.type_global) {
-			case EVENT_REGISTER:
-				std::cout << "EVENT_REGISTER GOT.\n";
-				return 0;
-			case EVENT_REGISTER_CANCEL:
-				std::cout << "EVENT_REGISTER_CANCEL GOT.\n";
-				connect_accept_cancel(msg, com);
-				return 2;
-			case EVENT_REGISTER_ACK:
-				std::cout << "EVENT_REGISTER_ACK GOT.\n";
-				connect_establish(msg, com);
-				return 0;
-			case EVENT_REGISTER_FAIL:
-				std::cout << "EVENT_REGISTER_FAIL GOT.\n";
-				connect_accept_fail(msg, com);
-				return 1;
-			case EVENT_UNREGISTER:
-				std::cout << "EVENT_UNREGISTER GOT.\n";
-				disconnect_accept(msg, com);
-				return 2;
-			case EVENT_UNREGISTER_CANCEL:
-				std::cout << "EVENT_UNREGISTER_CANCEL GOT.\n";
-				abort();
-				break;
-			case EVENT_UNREGISTER_ACK:
-				std::cout << "EVENT_UNREGISTER_ACK GOT.\n";
-				disconnect_done(msg, com);
-				return 2;
-			case EVENT_UNREGISTER_FAIL:
-				std::cout << "EVENT_UNREGISTER_FAIL GOT.\n";
-				abort();
-				break;
-			default:
-				return 0;
+	int process_specific_event(const eventbox::message& msg, communication& com) {
+		if (msg.type_global == EVENT_BASE_LAST) {
+			++counter;
 		}
-		return 1;
+//			std::cout << "Generic event recieved\n";
+		post_event(peer, message{ EVENT_BASE_LAST,0,0,0 });
+		return 0;
 	};
-	void register_incomming(communication* comm) {
+	void register_incomming(const eventbox::message& msg, communication& comm) {
 #ifndef NDEBUG
-		peer = { comm,comm->id };
+		peer = { &comm,comm.id };
 #else
 		peer = { comm };
 #endif
@@ -372,7 +601,9 @@ public:
 	test(size_t listen_size = 16): eventbox(listen_size) {};
 	connection_info peer;
 	bool initialized = false;
+	size_t counter = 0;
 };
 
+constexpr size_t size = sizeof(rigtorp::SPSCQueue<nullptr_t>);
 
 
--- TestEvent/main.cpp
+++ TestEvent/main.cpp
@@ -2,22 +2,23 @@
 
 #include <thread>
 
-test* boxes[2];
+test* boxes[2]{};
 std::thread threads[2];
 
 #include <intrin.h>
 
 void thread_func(int index) {
 	boxes[index] = new test();
-	std::this_thread::sleep_for(std::chrono::duration<double>(1));
-	test* peer = boxes[int(!bool(index))];
 	test* self = boxes[index];
+	while (!boxes[int(!bool(index))]);
+	test* peer = boxes[int(!bool(index))];
 	test::connection_info info;
 	auto t = std::chrono::high_resolution_clock::now();
 	uint64_t num = __rdtsc();
 	if (index) {
 		info = self->connect(peer, 0, 0);
 		self->peer = info;
+		self->initialized = true;
 //		self->connect_cancel(info);
 	}
 //	std::this_thread::sleep_for(std::chrono::duration<double>(1));
@@ -27,9 +28,13 @@ void thread_func(int index) {
 	if (index) {
 		while (true) {
 			self->process_events();
-			if (test::get_state(info) == test::comm_state::COMM_ACKED
-				|| test::get_state(info) == test::comm_state::COMM_EST)
+			if (test::get_state(info) == uint32_t(test::comm_state::COMM_ACKED)
+				|| test::get_state(info) == uint32_t(test::comm_state::COMM_EST))
 				break;
+		}
+		self->post_event(self->peer, test::message{ test::EVENT_BASE_LAST,0,0,0 });
+		while (__rdtsc() - num < 1000000000) {
+			self->process_events();
 		}
 		self->disconnect(info);
 		while (uint32_t(test::get_state(info))) {
@@ -46,16 +51,23 @@ void thread_func(int index) {
 			self->process_events();
 		}
 	}
-	std::cout << __rdtsc() - num << '\n';
+	std::cout << "Generic event processed:" << self->counter << '\n';
+	std::cout <<"Total time:"<< __rdtsc() - num << '\n';
 	delete boxes[index];
+	boxes[index] = nullptr;
 }
 
 int main() {
-	for (int i = 0; i < 2; ++i) {
-		threads[i] = std::move(std::thread(thread_func, i));
-	}
-	for (int i = 0; i < 2; ++i) {
-		threads[i].join();
+	while (true) {
+		mi_stats_reset();
+		for (int i = 0; i < 2; ++i) {
+			threads[i] = std::move(std::thread(thread_func, i));
+		}
+		for (int i = 0; i < 2; ++i) {
+			threads[i].join();
+		}
+//		mi_collect(true);
+		mi_stats_print(stdout);
 	}
 //	test test_init(100);
 	system("pause");
